21988a9e3ed7f7b7e8a0eb02550197d548aee616
Ben Hong <ben@bencodezen.io>
2024-03-13 08:23:16 -0700

    feat: add working bar chart example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+import Chart from 'chart.js/auto'
+
+const data = [
+  { year: 2010, count: 10 },
+  { year: 2011, count: 20 },
+  { year: 2012, count: 15 },
+  { year: 2013, count: 25 },
+  { year: 2014, count: 22 },
+  { year: 2015, count: 30 },
+  { year: 2016, count: 28 }
+]
+
+const elChart = ref<HTMLCanvasElement | null>(null)
+
+function initializeChart(el) {
+  new Chart(el, {
+    type: 'bar',
+    data: {
+      labels: data.map((row) => row.year),
+      datasets: [
+        {
+          label: 'Acquisitions by year',
+          data: data.map((row) => row.count)
+        }
+      ]
+    }
+  })
+}
+
+onMounted(() => {
+  const el = document.getElementById('chart')
+  initializeChart(el)
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <canvas ref="elChart" id="chart"></canvas>
   </main>
 </template>
